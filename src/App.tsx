d989e602ae31c45a05d5f8a74067237c52e58571
SIDMISHRA7 <sid94mishra@gmail.com>
2019-12-20 17:24:04 +0530

    react router initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router ,Switch,Route,Link} from 'react-router-dom'
+import './App.scss';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload test deploy to netlify .
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Projects" >Projects</Link></li>
+      </ul>
+      </div>
+      <Switch>        
+        <Route path="/Projects">
+        <div>Projects</div>
+        </Route>
+        <Route path="/">
+        <div>home</div>
+        </Route>
+      </Switch>
+    </Router>
+    
+    
   );
 }
 
